bb85de0db864cf89f5c11f90581e0523bd7094a0
yeshreddy <yeswanth.bhoomireddy@gmail.com>
2015-07-22 00:38:03 +0530

    commit1

---
+++ crackinterview/src/helper/twodmatrix.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author yeswanth
+ */
+public class twodmatrix {
+    
+    public void print(int arr[][]){
+        for (int[] arr1 : arr) {
+            for (int j = 0; j < arr1.length; j++) {
+                System.out.print(arr1[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ crackinterview/src/problems/Problem1_6.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problems;
+
+/**
+ *
+ * @author yeswanth
+ */
+public class Problem1_6 {
+    public void transform(int[][] matrix){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=1;(i+j)<matrix.length;j++){
+                int temp=matrix[i+j][i];
+                matrix[i+j][i]=matrix[i][i+j];
+                matrix[i][i+j]=temp;
+            }
+        }
+        
+        for(int i=0;i<matrix.length/2;i++){
+            int k=i;
+            int l=matrix.length-(k+1);
+            for(int j=0;j+k<matrix.length;j++){
+                int temp=matrix[j][k];
+                matrix[j][k]=matrix[j][l-k];
+                matrix[j][l-k]=temp;
+            }
+        }
+    }
+    
+}
